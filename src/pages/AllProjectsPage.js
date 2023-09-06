1076bdb482d4fe8b7c65d900c3bb5f24d3ae96b0
Vialoha <vi62469@gmail.com>
2023-09-06 12:50:40 +0100

    changes to the private

--- src/pages/AllProjectsPage.js
+++ src/pages/AllProjectsPage.js
@@ -7,7 +7,7 @@ const API_URL = "http://localhost:5005";
 function AllProjectsPage () {       
 
   const [projects, setProjects] = useState([]);
-     
+  const [searchTags, setSearchTags] = useState("");
   const storedToken = localStorage.getItem("authToken");
   
   const getAllProjects = () => {
@@ -27,11 +27,34 @@ function AllProjectsPage () {
   }, [] );
 
 
+  const filterProjectsByTags = (project) => {
+    if (!searchTags) {
+      return true; // No tags are entered in the search bar, show all projects
+    }
+    const projectTags = project.tags.map((tag) => tag.toLowerCase());
+    const searchTagsArray = searchTags.split(",");
+    return searchTagsArray.some((tag) =>
+      projectTags.includes(tag.trim().toLowerCase())
+    );
+  };
+
     return (
       <div>
         <br></br>
 
         <h1>Projects</h1>
+
+        <input
+        type="text"
+        placeholder="Search by tags (comma-separated)"
+        value={searchTags}
+        onChange={(e) => setSearchTags(e.target.value)}
+      />
+
+        <br></br>
+        <br></br>
+        <br></br>
+
         <Link to="/create-project"><button className="round-button">Add New Project</button></Link>
         
         <br></br>
@@ -39,7 +62,11 @@ function AllProjectsPage () {
         <br></br>
         <br></br>
 
-        {projects.map((project) => {
+        {projects
+        .filter(filterProjectsByTags)
+        .map((project) => {
+
+          if (project.private===false) {
           return (
             <div key={project._id} className="project">
               <h3>{project.title}</h3>
@@ -52,15 +79,11 @@ function AllProjectsPage () {
               <br></br>
             </div>
           );
+          }
         })}
       </div>
     );
-  
-
-
   };
  
-  
-  
  
 export default AllProjectsPage;
