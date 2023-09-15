8a9edaa201d1842352253f1ce53d7c31521d67c7
ThePainterThree <120008170+ThePainterThree@users.noreply.github.com>
2023-09-15 17:03:45 +0200

    Update api routes SignUpPage.js

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,15 +3,11 @@ import {
   MDBContainer,
 }
 from 'mdb-react-ui-kit';
-
 import Button from 'react-bootstrap/Button';
-
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
  
-// const API_URL = "http://localhost:5005";
-
 
 function SignUp() {
 
